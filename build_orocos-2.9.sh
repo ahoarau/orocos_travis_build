d3315839ff75d9344b90083ffe2c3701d82e1d9f
Antoine Hoarau <hoarau.robotics@gmail.com>
2017-03-20 00:59:42 +0100

    Update build_orocos-2.9.sh

--- build_orocos-2.9.sh
+++ build_orocos-2.9.sh
@@ -27,22 +27,23 @@ rosdep install -q --from-paths ~/orocos-2.9_ws/src --ignore-src --rosdistro $ROS
 catkin build -w ~/orocos-2.9_ws/ --summarize  --no-status
 
 tar -czf ~/orocos_toolchain-release.tar.gz ~/orocos-2.9_ws/install 
+curl --data '{"tag_name": "v2.9.0","target_commitish": "master","name": "v2.9.0","body": "Release of version 2.9.0","draft": false,"prerelease": false}' https://api.github.com/repos/:ahoarau/:orocos_travis_build/releases?access_token=:$API_TOKEN
 
-mkdir -p ~/rtt_ros-2.9_ws/src
-wstool init ~/rtt_ros-2.9_ws/src
-wstool merge https://github.com/kuka-isir/rtt_lwr/raw/rtt_lwr-2.0/lwr_utils/config/rtt_ros-2.9.rosinstall -t ~/rtt_ros-2.9_ws/src
-wstool update -t ~/rtt_ros-2.9_ws/src
+#mkdir -p ~/rtt_ros-2.9_ws/src
+#wstool init ~/rtt_ros-2.9_ws/src
+#wstool merge https://github.com/kuka-isir/rtt_lwr/raw/rtt_lwr-2.0/lwr_utils/config/rtt_ros-2.9.rosinstall -t ~/rtt_ros-2.9_ws/src
+#wstool update -t ~/rtt_ros-2.9_ws/src
 
-catkin config -w ~/rtt_ros-2.9_ws/ --init --install --extend ~/orocos-2.9_ws/install
-catkin config -w ~/rtt_ros-2.9_ws/ --cmake-args -DCMAKE_BUILD_TYPE=Release 
+#catkin config -w ~/rtt_ros-2.9_ws/ --init --install --extend ~/orocos-2.9_ws/install
+#catkin config -w ~/rtt_ros-2.9_ws/ --cmake-args -DCMAKE_BUILD_TYPE=Release 
 
-source ~/orocos-2.9_ws/install/setup.bash
-rosdep install -q --from-paths ~/rtt_ros-2.9_ws/src --ignore-src --rosdistro $ROS_DISTRO -y -r 
+#source ~/orocos-2.9_ws/install/setup.bash
+#rosdep install -q --from-paths ~/rtt_ros-2.9_ws/src --ignore-src --rosdistro $ROS_DISTRO -y -r 
 
-catkin build -w ~/rtt_ros-2.9_ws/ --summarize  --no-status
+#catkin build -w ~/rtt_ros-2.9_ws/ --summarize  --no-status
 
-tar -czf ~/rtt_ros-release.tar.gz ~/rtt_ros-2.9_ws/install 
+#tar -czf ~/rtt_ros-release.tar.gz ~/rtt_ros-2.9_ws/install 
 
-source ~/rtt_ros-2.9_ws/install/setup.bash
+#source ~/rtt_ros-2.9_ws/install/setup.bash
 
-cd $STARTPOINT
+#cd $STARTPOINT
