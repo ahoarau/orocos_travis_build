d1fe3f26c685a5ca8f985960c696b0c6af54366a
Antoine Hoarau <hoarau.robotics@gmail.com>
2017-03-19 20:46:07 +0100

    Update build_orocos-2.9.sh

--- build_orocos-2.9.sh
+++ build_orocos-2.9.sh
@@ -9,11 +9,6 @@ wstool init ~/orocos-2.9_ws/src
 wstool merge https://raw.githubusercontent.com/kuka-isir/rtt_lwr/rtt_lwr-2.0/lwr_utils/config/orocos_toolchain-2.9.rosinstall -t ~/orocos-2.9_ws/src
 wstool update -j2 -t ~/orocos-2.9_ws/src
 
-mkdir -p ~/rtt_ros-2.9_ws/src
-wstool init ~/rtt_ros-2.9_ws/src
-wstool merge https://github.com/kuka-isir/rtt_lwr/raw/rtt_lwr-2.0/lwr_utils/config/rtt_ros-2.9.rosinstall -t ~/rtt_ros-2.9_ws/src
-wstool update -j2 -t ~/rtt_ros-2.9_ws/src
-
 # Get the latest updates
 
 cd ~/orocos-2.9_ws/src/orocos_toolchain
@@ -25,17 +20,26 @@ git submodule foreach git pull
 catkin config --init -w ~/orocos-2.9_ws/ --install --extend /opt/ros/$ROS_DISTRO
 catkin config -w ~/orocos-2.9_ws/ --cmake-args -DCMAKE_BUILD_TYPE=Release
 
-catkin config -w ~/rtt_ros-2.9_ws/ --init --install --extend ~/orocos-2.9_ws/install
-catkin config -w ~/rtt_ros-2.9_ws/ --cmake-args -DCMAKE_BUILD_TYPE=Release 
-
-
 rosdep install -q --from-paths ~/orocos-2.9_ws/src --ignore-src --rosdistro $ROS_DISTRO -y
-rosdep install -q --from-paths ~/rtt_ros-2.9_ws/src --ignore-src --rosdistro $ROS_DISTRO -y
+
 
 
 # Build
-catkin build -w ~/orocos-2.9_ws/src
 catkin build -w ~/orocos-2.9_ws/
+
+
+
+mkdir -p ~/rtt_ros-2.9_ws/src
+wstool init ~/rtt_ros-2.9_ws/src
+wstool merge https://github.com/kuka-isir/rtt_lwr/raw/rtt_lwr-2.0/lwr_utils/config/rtt_ros-2.9.rosinstall -t ~/rtt_ros-2.9_ws/src
+wstool update -j2 -t ~/rtt_ros-2.9_ws/src
+
+catkin config -w ~/rtt_ros-2.9_ws/ --init --install --extend ~/orocos-2.9_ws/install
+catkin config -w ~/rtt_ros-2.9_ws/ --cmake-args -DCMAKE_BUILD_TYPE=Release 
+
+rosdep install -q --from-paths ~/rtt_ros-2.9_ws/src --ignore-src --rosdistro $ROS_DISTRO -y
+
+catkin build -w ~/rtt_ros-2.9_ws/
 
 source ~/rtt_ros-2.9_ws/install/setup.bash
 
