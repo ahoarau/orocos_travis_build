aa402d593cb1ae4878a47394a26d950714888ab5
Antoine Hoarau <hoarau.robotics@gmail.com>
2017-03-20 19:36:04 +0100

    Update build_orocos-2.9.sh

--- build_orocos-2.9.sh
+++ build_orocos-2.9.sh
@@ -2,8 +2,6 @@
 
 STARTPOINT=$(pwd)
 echo "Starting point : " $(pwd)
-echo "Super secret : $0"
-echo $2
 
 mkdir -p ~/orocos-2.9_ws/src
 
@@ -33,13 +31,13 @@ tar -czf ~/orocos_toolchain-release.tar.gz ~/orocos-2.9_ws/install
 git config --global user.email "hoarau.robotics@gmail.com"
 git config --global user.name "Antoine Hoarau - Travis-CI"
 
-git clone https://ahoarau:$1@github.com/ahoarau/orocos_travis_build > /dev/null
+git clone https://ahoarau:$1@github.com/ahoarau/orocos_travis_build
 cd orocos_travis_build
 git checkout -b $(lsb_release -cs)-release
 cp ~/orocos_toolchain-release.tar.gz .
 git add orocos_toolchain-release.tar.gz
 git commit -m "Travis-CI build $(date)"
-git push origin $(lsb_release -cs)-release > /dev/null
+git push -q origin $(lsb_release -cs)-release
 
 #curl --data '{"tag_name": "v2.9.0","target_commitish": "master","name": "v2.9.0","body": "Release of version 2.9.0","draft": false,"prerelease": false}' https://api.github.com/repos/:ahoarau/:orocos_travis_build/releases?access_token=:$API_TOKEN
 
