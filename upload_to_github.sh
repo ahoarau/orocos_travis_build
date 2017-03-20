893d221c6793811b64e3f0badb26f269797d6542
Antoine Hoarau <hoarau.robotics@gmail.com>
2017-03-20 20:17:32 +0100

    Update upload_to_github.sh

--- upload_to_github.sh
+++ upload_to_github.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-orocosfilename=orocos_toolchain-$(lsb_release -cs)-$(uname -m)-release.tar.gz
-
-tar -czf ~/$orocosfilename.tar.gz ~/orocos-2.9_ws/install
+tar -czf ~/orocos_toolchain-release.tar.gz ~/orocos-2.9_ws/install
 
 git config --global user.email "hoarau.robotics@gmail.com"
 git config --global user.name "Antoine Hoarau - Travis-CI"
@@ -10,8 +8,8 @@ git config --global user.name "Antoine Hoarau - Travis-CI"
 git clone https://ahoarau:$1@github.com/ahoarau/orocos_travis_build
 cd orocos_travis_build
 git checkout -q -b $(lsb_release -cs)-release
-cp ~/$orocosfilename.tar.gz .
-git add ~/$orocosfilename.tar.gz
+cp ~/orocos_toolchain-release.tar.gz .
+git add ~/orocos_toolchain-release.tar.gz
 git commit -m "Travis-CI build $(date)"
 git push -q origin $(lsb_release -cs)-release
 
