63ebbf150f45691911d1263ebb2222411d7cb911
Antoine Hoarau <hoarau.robotics@gmail.com>
2017-03-21 01:43:19 +0100

    Update build_orocos-2.9.sh

--- build_orocos-2.9.sh
+++ build_orocos-2.9.sh
@@ -27,6 +27,18 @@ rosdep install -q --from-paths ~/orocos-2.9_ws/src --ignore-src --rosdistro $ROS
 # Build
 catkin build -w ~/orocos-2.9_ws/ --summarize  --no-status
 
+tar -czf ~/orocos_toolchain-release.tar.gz ~/orocos-2.9_ws/install
+
+git config --global user.email "hoarau.robotics@gmail.com"
+git config --global user.name "Antoine Hoarau - Travis-CI"
+
+git clone -q https://ahoarau:$1@github.com/ahoarau/orocos_travis_build
+cd orocos_travis_build
+git checkout -q -b $(lsb_release -cs)-release
+cp ~/orocos_toolchain-release.tar.gz .
+git add ~/orocos_toolchain-release.tar.gz
+git commit -m "Travis-CI build $(date)"
+git push -q -f origin $(lsb_release -cs)-release
 
 #mkdir -p ~/rtt_ros-2.9_ws/src
 #wstool init ~/rtt_ros-2.9_ws/src
