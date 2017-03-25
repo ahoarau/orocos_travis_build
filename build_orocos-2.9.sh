8fe5a4349b210e1d4369ea6bbaf8373d2078b55f
Antoine Hoarau <hoarau.robotics@gmail.com>
2017-03-25 13:05:45 +0100

    Update build_orocos-2.9.sh

--- build_orocos-2.9.sh
+++ build_orocos-2.9.sh
@@ -24,6 +24,10 @@ rm ~/orocos-2.9_ws/src/orocos_toolchain/orogen/manifest.xml
 catkin config --init -w ~/orocos-2.9_ws/ --install --extend /opt/ros/$ROS_DISTRO
 catkin config -w ~/orocos-2.9_ws/ --cmake-args -DCMAKE_BUILD_TYPE=Release -DENABLE_CORBA=ON -DCORBA_IMPLEMENTATION=OMNIORB
 
+if [ $ROS_DISTRO == "hydro" ]; then
+    apt-get install ruby1.9.1-dev
+fi
+
 rosdep install -q --from-paths ~/orocos-2.9_ws/src --ignore-src --rosdistro $ROS_DISTRO -y -r 
 
 # Build
