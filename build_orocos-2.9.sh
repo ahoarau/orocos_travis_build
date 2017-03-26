21fea3c5c66509989a16d34a6ae032ff9a4db0d3
Antoine Hoarau <hoarau.robotics@gmail.com>
2017-03-26 17:42:38 +0200

    Update build_orocos-2.9.sh

--- build_orocos-2.9.sh
+++ build_orocos-2.9.sh
@@ -28,7 +28,7 @@ rosdep install -q --from-paths ~/orocos-2.9_ws/src --ignore-src --rosdistro $ROS
 
 if [ "$ROS_DISTRO" = "hydro" ]; then
     apt-get install -q -y ruby1.9.1-dev
-    rvm --default use 1.9.3
+    update-alternatives --set /usr/bin/ruby ruby /usr/bin/ruby1.9.1 10
 fi
 
 # Build
